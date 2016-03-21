f01b419dea973953abe1de466062308a8144d732
umbrae <umbrae@reddit.com>
2016-03-21 14:18:16 -0700

    Events: Add message event

--- reddit_robin/controllers.py
+++ reddit_robin/controllers.py
@@ -1,7 +1,9 @@
 import datetime
 import posixpath
+from datetime import datetime
 
 import pytz
+from pylons import request
 from pylons import app_globals as g
 from pylons import tmpl_context as c
 
@@ -23,6 +25,7 @@ from r2.lib.validator import (
 )
 from r2.models import Account
 
+from . import events
 from .validators import VRobinRoom
 from .pages import (
     RobinAdmin,
@@ -189,8 +192,6 @@ class RobinController(RedditController):
         if form.has_errors("message", errors.NO_TEXT, errors.TOO_LONG):
             return
 
-        # if we decide we want logging, perhaps we can make a logger that
-        # watches the amqp bus instead of complicating this request logic?
         websockets.send_broadcast(
             namespace="/robin/" + room.id,
             type="chat",
@@ -199,6 +200,15 @@ class RobinController(RedditController):
                 "body": message,
             },
         )
+
+        events.message(
+            room=room,
+            message=message,
+            sent_dt=datetime.utcnow(),
+            context=c,
+            request=request,
+        )
+
 
     @validatedForm(
         VUser(),
